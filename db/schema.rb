9d60d75e9e074122d543c79fd7602ca3e75341ba
Johan Watelet <johanwatelet@gmail.com>
2024-03-27 22:27:19 +0100

    Add flight model and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_185438) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_203415) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -19,4 +19,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_27_185438) do
     t.index ["code"], name: "unique_code_airport_idx", unique: true
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.integer "departure_airport_id", null: false
+    t.integer "arrival_airport_id", null: false
+    t.time "duration"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,21 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Airport.create(code: 'ATL', name: 'Hartsfield–Jackson Atlanta International Airport')
-Airport.create(code: 'DFW', name: 'Dallas/Fort Worth International Airport')
-Airport.create(code: 'DEN', name: 'Denver International Airport')
-Airport.create(code: 'ORD', name: 'O\'Hare International Airport')
-Airport.create(code: 'JFK', name: 'John F. Kennedy International Airport')
-Airport.create(code: 'MIA', name: 'Miami International Airport')
+atl_airport = Airport.create(code: 'ATL', name: 'Hartsfield–Jackson Atlanta International Airport')
+dfw_airport = Airport.create(code: 'DFW', name: 'Dallas/Fort Worth International Airport')
+den_airport = Airport.create(code: 'DEN', name: 'Denver International Airport')
+ord_airport = Airport.create(code: 'ORD', name: 'O\'Hare International Airport')
+jfk_airport = Airport.create(code: 'JFK', name: 'John F. Kennedy International Airport')
+mia_airport = Airport.create(code: 'MIA', name: 'Miami International Airport')
+
+Flight.create(departure_airport: atl_airport, arrival_airport: dfw_airport, duration: 4.hours, date: Date.strptime("30-04-2024", "%d-%m-%Y"))
+Flight.create(departure_airport: atl_airport, arrival_airport: den_airport, duration: 7.hours, date: Date.strptime("01-05-2024", "%d-%m-%Y"))
+
+Flight.create(departure_airport: dfw_airport, arrival_airport: atl_airport, duration: 4.hours, date: Date.strptime("02-05-2024", "%d-%m-%Y"))
+Flight.create(departure_airport: dfw_airport, arrival_airport: ord_airport, duration: 1.hours, date: Date.strptime("03-05-2024", "%d-%m-%Y"))
+
+Flight.create(departure_airport: jfk_airport, arrival_airport: ord_airport, duration: 6.hours, date: Date.strptime("04-05-2024", "%d-%m-%Y"))
+Flight.create(departure_airport: jfk_airport, arrival_airport: mia_airport, duration: 8.hours, date: Date.strptime("05-05-2024", "%d-%m-%Y"))
+
+Flight.create(departure_airport: mia_airport, arrival_airport: atl_airport, duration: 8.hours, date: Date.strptime("06-05-2024", "%d-%m-%Y"))
+Flight.create(departure_airport: mia_airport, arrival_airport: jfk_airport, duration: 2.hours, date: Date.strptime("06-05-2024", "%d-%m-%Y"))
