1b088764ed0bfad954eeeb81a9c14ba220ff6da2
Johan Watelet <johanwatelet@gmail.com>
2024-04-05 12:12:45 +0200

    Add booking and passenger model and migration

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,7 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
   has_many :bookings
-  has_many :passengers, though: :bookings
+  has_many :passengers, through: :bookings
 
   def date_formatted
     date.strftime("%d/%m/%Y")
