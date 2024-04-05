51afb5a43c4a24f48f76f0491a83b7629250407b
Johan Watelet <johanwatelet@gmail.com>
2024-04-05 16:41:20 +0200

    Add form for passengers and booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,41 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @passengers_num = set_passenger_num
+    @flight = Flight.find(selected_flight_params[:flight_id])
+    @passengers_num.times { @booking.passengers.build }
+  end
+
+  def show
+    @booking = Booking.includes(:passengers, flight: [:arrival_airport, :departure_airport])
+                      .find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      @flight = Flight.find(booking_params[:flight_id])
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_passenger_num
+    if selected_flight_params[:passengers].to_i <= 4
+      selected_flight_params[:passengers].to_i
+    else
+      1
+    end
+  end
+
+  def selected_flight_params
+    params.permit(:flight_id, :passengers, :commit)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:firstname, :lastname, :email])
   end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,8 @@
 class Passenger < ApplicationRecord
   belongs_to :booking
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
