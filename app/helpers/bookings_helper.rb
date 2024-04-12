6e9451df1c34d0f8f5b9d8ee061b7e49a473cd6d
Johan Watelet <johanwatelet@gmail.com>
2024-04-12 11:24:33 +0200

    refactor code, create partial and uses helper

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,13 @@
 module BookingsHelper
+  def add_passenger(booking)
+    link_to new_booking_path(flight_id: booking.flight_id, passengers: booking.passengers.length + 1), class: "add" do
+      image_tag 'account-plus.svg'
+    end
+  end
+
+  def remove_passenger(booking)
+    link_to new_booking_path(flight_id: booking.flight_id, passengers: booking.passengers.length - 1), class: "remove" do
+      image_tag 'account-remove.svg'
+    end
+  end
 end
